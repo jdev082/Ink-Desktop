2ff8d8f6436079eabb51311a359183469d4ebbbb
JaydenDev <willsjayden2@gmail.com>
2022-06-13 12:49:17 -0500

    Update Ink

--- update-ink.sh
+++ update-ink.sh
@@ -2,7 +2,7 @@
 # Ink-Desktop Ink Updater Script
 # JaydenDev Software 2022 MIT License
 rm -rf Ink
-git clone --recursive-submodule https://github.com/Darth-Ness/Ink
+git clone https://github.com/Darth-Ness/Ink
 git add -A
 git commit -m "Update Ink"
 git push
